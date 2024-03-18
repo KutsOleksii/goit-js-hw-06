d03d65d6f0af4020a0503d5c18dd01526a7638d1
Oleksii Kuts <hapt.com.ua@gmail.com>
2024-03-18 16:29:15 +0200

    task 02 complete

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const liItems = ingredients.map((item) => {
+  let element = document.createElement("li");
+  element.textContent = item;
+  element.class = "item";
+  return element;
+});
+
+document.querySelector('ul#ingredients').append(...liItems);
