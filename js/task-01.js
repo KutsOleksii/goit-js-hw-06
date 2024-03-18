815f68c0abca05b6960b2c196ec89c89628fd74f
Oleksii Kuts <hapt.com.ua@gmail.com>
2024-03-18 16:27:02 +0200

    task 01 complete

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,6 @@
-console.log(`Number of categories: ${numberOfCategories}`);
+const categories = document.querySelectorAll("li.item");
+console.log(`Number of categories: ${categories.length}`);
+categories.forEach(function(item, index) {
+  console.log(`Category: ${item.querySelector("h2").textContent}`)
+  console.log(`Elements: ${item.querySelectorAll("ul li").length}`)
+})
